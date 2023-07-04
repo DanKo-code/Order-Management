f36dc72375e34738a0d312447fd3f7baf455495d
Tanki1629762003 <96771888+DanKo-code@users.noreply.github.com>
2023-07-04 18:01:37 +0300

    make list temolate

---
+++ Order-Management/force-app/main/default/lwc/oMList/oMList.js
@@ -0,0 +1,30 @@
+import { LightningElement } from 'lwc';
+
+export default class OMList extends LightningElement {
+    products = [
+        {
+            "id": "1"
+        },
+        {
+            "id": "1"
+        },
+        {
+            "id": "1"
+        },
+        {
+            "id": "1"
+        },
+        {
+            "id": "1"
+        },
+        {
+            "id": "1"
+        },
+        {
+            "id": "1"
+        },
+        {
+            "id": "1"
+        },
+    ]
+}
