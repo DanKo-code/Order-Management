5f78c8fbcaf156b10a170c4243de5cf5277e1755
Tanki1629762003 <96771888+DanKo-code@users.noreply.github.com>
2023-07-04 11:47:58 +0300

    Start write header

---
+++ Order-Management/force-app/main/default/lwc/oMHeader/oMHeader.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class OMHeader extends LightningElement {}
---
+++ Order-Management/force-app/main/default/lwc/oMWrapper/oMWrapper.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class OMWrapper extends LightningElement {}
