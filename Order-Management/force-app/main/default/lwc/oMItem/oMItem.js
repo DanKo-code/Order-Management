2554cebb238a42623897c5a3d548cae9f871deb8
Tanki1629762003 <96771888+DanKo-code@users.noreply.github.com>
2023-07-05 19:54:28 +0300

    add toast when add to cart

--- Order-Management/force-app/main/default/lwc/oMItem/oMItem.js
+++ Order-Management/force-app/main/default/lwc/oMItem/oMItem.js
@@ -1,4 +1,5 @@
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
 export default class OMItem extends LightningElement {
     @api product;
@@ -9,6 +10,19 @@ export default class OMItem extends LightningElement {
             bubbles: true
         });
 
+        this.dispatchEvent(event);
+    }
+
+    handleCartAdd() {
+        this.showToast();
+    }
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Alert!',
+            message: 'Product added to cart',
+            variant: 'success'
+        });
         this.dispatchEvent(event);
     }
 }
