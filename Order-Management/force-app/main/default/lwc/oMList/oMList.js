d701e13cecb96215d474a9e292dc283ce2a36556
Tanki1629762003 <96771888+DanKo-code@users.noreply.github.com>
2023-07-06 06:42:58 +0300

    create custom object Product and fields to it, get records from db with wire decorator

--- Order-Management/force-app/main/default/lwc/oMList/oMList.js
+++ Order-Management/force-app/main/default/lwc/oMList/oMList.js
@@ -3,63 +3,83 @@ import { subscribe, MessageContext } from 'lightning/messageService';
 import FILTER_UPDATED_CHANNEL from '@salesforce/messageChannel/Filter_Update__c';
 import SEND_PRODUCT_CHANNEL from '@salesforce/messageChannel/Send_Product__c';
 
+import getProductList from '@salesforce/apex/ProductController.getProductList'
+
 
 export default class OMList extends LightningElement {
 
     //TEMP DB
-    @track products = [
-        {
-            "id": "1",
-            'productName': 'Car',
-            'Description': `1 ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
-            dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
-            niolore et saepe, magnam perspiciatis ut incidunt est.`,
-            Type: 'firstType',
-            Family: 'firstFamily',
-            img: 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/dynamox1.jpg',
-            price: 100,
-        },
-        {
-            "id": "2",
-            'productName': 'Dog',
-            'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
-            dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
-            niolore et saepe, magnam perspiciatis ut incidunt est.`,
-            Type: 'firstType',
-            Family: 'firstFamily',
-            price: 100,
-        },
-        {
-            "id": "3",
-            'productName': 'Nikita',
-            'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
-            dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
-            niolore et saepe, magnam perspiciatis ut incidunt est.`,
-            Type: 'secondType',
-            Family: 'secondFamily',
-            price: 100,
-        },
-        {
-            "id": "4",
-            'productName': 'TV',
-            'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
-            dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
-            niolore et saepe, magnam perspiciatis ut incidunt est.`,
-            Type: 'secondType',
-            Family: 'secondFamily',
-            price: 100,
-        },
-        {
-            "id": "4",
-            'productName': 'Door',
-            'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
-            dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
-            niolore et saepe, magnam perspiciatis ut incidunt est.`,
-            Type: 'thirdType',
-            Family: 'thirdFamily',
-            price: 100,
+    // @track products = [
+    //     {
+    //         "id": "1",
+    //         'productName': 'Car',
+    //         'Description': `1 ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
+    //         dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
+    //         niolore et saepe, magnam perspiciatis ut incidunt est.`,
+    //         Type: 'firstType',
+    //         Family: 'firstFamily',
+    //         img: 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/dynamox1.jpg',
+    //         price: 100,
+    //     },
+    //     {
+    //         "id": "2",
+    //         'productName': 'Dog',
+    //         'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
+    //         dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
+    //         niolore et saepe, magnam perspiciatis ut incidunt est.`,
+    //         Type: 'firstType',
+    //         Family: 'firstFamily',
+    //         price: 100,
+    //     },
+    //     {
+    //         "id": "3",
+    //         'productName': 'Nikita',
+    //         'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
+    //         dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
+    //         niolore et saepe, magnam perspiciatis ut incidunt est.`,
+    //         Type: 'secondType',
+    //         Family: 'secondFamily',
+    //         price: 100,
+    //     },
+    //     {
+    //         "id": "4",
+    //         'productName': 'TV',
+    //         'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
+    //         dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
+    //         niolore et saepe, magnam perspiciatis ut incidunt est.`,
+    //         Type: 'secondType',
+    //         Family: 'secondFamily',
+    //         price: 100,
+    //     },
+    //     {
+    //         "id": "4",
+    //         'productName': 'Door',
+    //         'Description': `Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus repudiandae, praesentium nostrum alias
+    //         dignissimos cum aliquid.Delectus molestiae adipisci aliquam, repellat voluptas nulla assumenda odio nobis
+    //         niolore et saepe, magnam perspiciatis ut incidunt est.`,
+    //         Type: 'thirdType',
+    //         Family: 'thirdFamily',
+    //         price: 100,
+    //     }
+    // ];
+
+    @track products = []
+    @track errors
+
+    //Bad method???
+    @wire(getProductList)
+    wiredPlayerList({ data, error }) {
+        if (data) {
+            this.products = data;
         }
-    ];
+        else if (error) {
+            this.errors = error;
+            alert(error)
+        }
+    }
+    //Bad method???
+
+
 
     //"REDUX"
     type = '';
@@ -103,8 +123,8 @@ export default class OMList extends LightningElement {
 
     @api inputText = '';
     get filteredItems() {
-        return this.products.filter(product => product.productName.toLowerCase().includes(this.inputText.toLowerCase()) ||
-            product.Description.toLowerCase().includes(this.inputText.toLowerCase())).filter(product => product.Type.includes(this.type) && product.Family.includes(this.family));
+        return this.products.filter(product => product.Name.toLowerCase().includes(this.inputText.toLowerCase()) ||
+            product.Description__c.toLowerCase().includes(this.inputText.toLowerCase())).filter(product => product.Type__c.includes(this.type) && product.Family__c.includes(this.family));
     }
 
 
